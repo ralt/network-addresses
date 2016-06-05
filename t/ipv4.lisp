e02621f46f20fbf1a06feb28703b83230ef49983
Florian MARGAINE <florian@margaine.com>
2016-06-05 22:44:38 +0200

    Sanity checks.

--- t/ipv4.lisp
+++ t/ipv4.lisp
@@ -14,3 +14,8 @@
   (signals-invalid-format (na4:make-network-from-cidr "100.100.100.100"))
   (signals-invalid-format (na4:make-network-from-cidr "300.100.100.100/24"))
   (signals-invalid-format (na4:make-network-from-cidr "100.100.100.100/33")))
+
+(test sanity
+  (let ((network (na4:make-network-from-cidr "255.255.255.255/32")))
+    (is (= (na:subnet-length network) 32))
+    (is (= (na:integer-value network) 4294967295))))
